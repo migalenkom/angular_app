4df42830125440d698ae3509d72ee88966a79d44
Mike Mygalenko <m.mygalenko@svitla.com>
2016-03-11 17:13:38 +0200

    hometask 2 done

--- Hometask#2/js/main/controllers/MainController.js
+++ Hometask#2/js/main/controllers/MainController.js
@@ -2,4 +2,21 @@ App.controller('MainController',function($scope, toaster) {
   $scope.pop = function(){
       toaster.pop('success', "title", "text");
   };
+
+  $scope.people = [];
+
+  $scope.insert = function(human){
+  	
+  	$scope.people.push(human);
+  	toaster.pop('success', "", "User was added successfully");
+
+  };
+  
+  $scope.delete = function(index){
+  		
+  	$scope.people.splice(index, 1);
+  	toaster.pop('info', "", "User was deleted successfully");  
+  
+  };
+
 });
